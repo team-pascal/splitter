9e7d39402629a606fb66e8cd69d5a1c17270c281
s1270146 <s1270146@gmail.com>
2024-05-22 20:05:05 +0900

    feat: user model

---
+++ backend/app/src/internal/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+type User struct {
+	bun.BaseModel `bun:"table:users"`
+
+	ID        uuid.UUID `bun:"id,pk,type:uuid"`
+	Name      string    `bun:"name,notnull"`
+	GroupID   uuid.UUID `bun:"group_id,type:uuid"`
+	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	DeletedAt bun.NullTime
+
+	Group *Group `bun:"rel:belongs-to,join:group_id=id"`
+}
